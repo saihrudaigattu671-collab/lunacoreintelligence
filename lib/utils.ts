93018a5dc5812b0b69335345838c502d8eaf2c33
saihrudaigattu88-design <saihrudaigattu88@gmail.com>
2026-08-15 16:07:51 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+/**
+ * Combines and cleans up CSS class names.
+ * It prevents duplicate or conflicting styling rules from clashing.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
